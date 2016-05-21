ebb632bb8b4b12d7360872dc1349aff0d81a792f
yaimapp <gmonday730-mbp@yahoo.co.jp>
2016-05-21 13:15:12 +0900

    [mod]copy tmux files

--- install.sh
+++ install.sh
@@ -22,3 +22,16 @@ do
     echo $f
     [ ! -e $HOME/.vim/$f ] && ln -s $HOME/.dotfiles/.vim/$f $HOME/.vim/$f
 done
+
+tmuxdir=~/.tmux
+[ ! -e $tmuxdir ] && mkdir -p $tmuxdir
+cd ~/.dotfiles/.tmux
+tmuxfiles="*"
+for f in ${tmuxfiles}
+do
+  [[ $f == ".git" ]] && continue
+  [[ $f == ".DS_Store" ]] && continue
+  [[ $f == ".gitignore" ]] && continue
+  echo $f
+  [ ! -e $tmuxdir/$f ] && cp -i $HOME/.dotfiles/.tmux/$f $tmuxdir/$f
+done
